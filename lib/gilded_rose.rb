3804db2e2bbd14641450b265e086398d888e7a9b
Thomas Ross <thomasross3610@gmail.com>
2019-10-22 13:53:22 +0100

    max/min value if statements refactored.New #enforce_min_and_max_qualities private method created.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,27 +12,19 @@ MIN_QUALITY = 0
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > MIN_QUALITY
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
-        end
       else
-        if item.quality < MAX_QUALITY
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
-              end
             end
             if item.sell_in < 6
-              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
-              end
             end
           end
-        end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
@@ -40,20 +32,24 @@ MIN_QUALITY = 0
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > MIN_QUALITY
               if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
               end
-            end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < MAX_QUALITY
             item.quality = item.quality + 1
-          end
         end
       end
+      enforce_min_and_max_qualities(item)
     end
+  end
+
+private
+
+  def enforce_min_and_max_qualities(item)
+    item.quality < MIN_QUALITY ? item.quality = MIN_QUALITY : item.quality = item.quality
+    item.quality > MAX_QUALITY ? item.quality = MAX_QUALITY : item.quality = item.quality
   end
 end
