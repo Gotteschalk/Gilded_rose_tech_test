09af3045a83a369e4eb605ddb3b7398251020e0b
Thomas Ross <thomasross3610@gmail.com>
2019-10-22 12:20:45 +0100

    Testing framework started for standard items.  Three tests added and passed: update qulity method reduces quality by 1, reduces Sellin value by 1, and cannot result in negative quality values

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,26 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    context "When called on a standard item"
+
+      items = [Item.new("foo", 5, 10)]
+      GildedRose.new(items).update_quality
+
+      it "reduces the quality by 1" do
+        expect(items[0].quality).to eq 9
+      end
+
+      it "reduces the SellIn value by 1" do
+        expect(items[0].sell_in).to eq 4
+      end
+
+      it "will not reduce the quality to less than 0" do
+        11.times{ GildedRose.new(items).update_quality }
+      expect(items[0].quality).to eq 0
     end
   end
+  
 
 end
