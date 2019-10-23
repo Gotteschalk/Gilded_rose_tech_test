1aa93ca501fc927173ae3385eba153294c79d964
Thomas Ross <thomasross3610@gmail.com>
2019-10-23 11:39:38 +0100

    Refactored into items as subordinate classes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,60 +1,102 @@
-require_relative 'item'
-require_relative 'item_category'
+require 'delegate'
 
 class GildedRose
-
-MAXIMUM_QUALITY_VALUE = 50
-MINIMUM_QUALITY_VALUE = 0
-STD_QUALITY_DEPRECIATION = 1
-STD_QUALITY_APPRECIATION = 1
+attr_accessor :items
 
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.depreciates?
-        decrease_quality(item)
-      elsif item.appreciates?
-        increase_quality(item)
-      end
-      enforce_min_and_max_quality(item)
-      decrease_sell_in_date(item)
+      AllItems.wrap(item).update
+    end
+  end
+end
+
+class AllItems < SimpleDelegator
+
+  def self.wrap(item)
+    case item.name
+    when "Aged Brie"
+      AgedBrie.new(item)
+    when "Sulfuras, Hand of Ragnaros"
+      SulfurasItem.new(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      BackstagePass.new(item)
+    when "Conjured Mana Cake"
+      ConjuredItem.new(item)
+    else
+      new(item)
     end
   end
 
-private
-
-  def enforce_min_and_max_quality(item)
-    return if item.name == "Sulfuras, Hand of Ragnaros"
-      item.quality < MINIMUM_QUALITY_VALUE ? item.quality = MINIMUM_QUALITY_VALUE : item.quality = item.quality
-      item.quality > MAXIMUM_QUALITY_VALUE ? item.quality = MAXIMUM_QUALITY_VALUE : item.quality = item.quality
-
+  def update
+    adjust_sell_in_date
+    adjust_quality
   end
 
-  def decrease_quality(item)
-    item.sell_in > 0 ? item.quality -= STD_QUALITY_DEPRECIATION : item.quality -= 2 * STD_QUALITY_DEPRECIATION
-    if item.name == "Conjured Mana Cake"
-      item.sell_in > 0 ? item.quality -= STD_QUALITY_DEPRECIATION : item.quality -= 2 * STD_QUALITY_DEPRECIATION
-    end
+#For standard Items
+  def adjust_sell_in_date
+    self.sell_in -= 1
   end
 
-  def increase_quality(item)
-    item.quality += STD_QUALITY_APPRECIATION
-    if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in <= 0
-        item.quality = MINIMUM_QUALITY_VALUE
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in <= 5
-          item.quality += 2 * STD_QUALITY_APPRECIATION
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in <= 10
-      item.quality += STD_QUALITY_APPRECIATION
-    end
+  def adjust_quality
+    self.sell_in < 0 ? self.quality -= 2 : self.quality -= 1
+    self.quality < 0 ? self.quality = 0 : self.quality = self.quality
+  end
+end
+
+#Below Classes for special cases
+class AgedBrie < AllItems
+  def adjust_quality
+    self.quality += 1
+    self.quality > 50 ? self.quality = 50 : self.quality = self.quality
+  end
+end
+
+class SulfurasItem < AllItems
+  def adjust_quality
+    # do nothing
   end
 
-  def decrease_sell_in_date(item)
-    unless item.name == "Sulfuras, Hand of Ragnaros"
-      item.sell_in -= 1
+  def adjust_sell_in_date
+    # do nothing
+  end
+end
+
+class BackstagePass < AllItems
+  def adjust_quality
+    if self.sell_in < 0
+      self.quality = 0
+    elsif self.sell_in < 5
+      self.quality += 3
+    elsif self.sell_in < 10
+      self.quality += 2
+    else
+      self.quality += 1
     end
+  end
+end
+
+class ConjuredItem < AllItems
+  def adjust_quality
+    self.sell_in < 0 ? self.quality -= 4 : self.quality -= 2
+    self.quality < 0 ? self.quality = 0 : self.quality = self.quality
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s()
+    "#{@name}, #{@sell_in}, #{@quality}"
   end
 
 end
